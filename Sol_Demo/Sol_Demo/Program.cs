65410da66d88ae23ecfb009110638d1f80467a1b
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-17 18:12:25 +0530

    Code Commit

--- Sol_Demo/Sol_Demo/Program.cs
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -11,6 +11,25 @@ namespace Sol_Demo
             IDemo demo = new Demo();
             demo.DemoMethod1();
             demo.DemoDefaultMethod();
+
+            IA demoA = new DemoC();
+            demoA.Display();
+            demoA.Display1();
+
+            IB demoB = new DemoC();
+            demoB.Display();
+            demoB.Display2();
+
+            ID dObj = new DemoD();
+            dObj.Display1();
+            dObj.Display2();
+
+            demoA = dObj;
+            demoA.Display();
+
+            demoB = dObj;
+            demoB.Display();
+
         }
     }
 
@@ -37,4 +56,49 @@ namespace Sol_Demo
         //    Console.WriteLine("Override Default Method Demo");
         //}
     }
+
+
+    public interface IA
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display A");
+        }
+
+        public void Display1()
+        {
+            Console.WriteLine("Display1 : A");
+        }
+    }
+
+    public interface IB
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display B");
+        }
+
+        public void Display2()
+        {
+            Console.WriteLine("Display2 : A");
+        }
+    }
+
+
+    public class DemoC : IA,IB
+    {
+
+    }
+
+
+    public interface ID : IA,IB
+    {
+
+    }
+
+    public class DemoD : ID
+    {
+        
+    }
+
 }
